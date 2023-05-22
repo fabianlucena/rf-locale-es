32230c3067acf67c124c2f2bff298f4438d01960
Fabian Lucena <fabianlucena@gmail.com>
2023-05-22 11:12:39 -0300

    Package normalization.

--- rf-locale-es.js
+++ rf-locale-es.js
@@ -2,7 +2,7 @@
 
 import es from './locale/es.js';
 import esAr from './locale/es-AR.js';
-import Locale from 'rf-locale';
+import {Locale} from 'rf-locale';
 
 Locale.availableLanguages.es = es;
 Locale.availableLanguages['es-AR'] = esAr;
